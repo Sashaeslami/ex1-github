4d71158462edb01979caf94f2ac8ab37f2bb0b03
Sasha Eslami <Sasha.eslami@gmail.com>
2017-04-13 18:15:17 -0700

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,6 +1,6 @@
-print "how many girls have you had?",
+print "how many dogs have you had?",
 girls = raw_input()
-print "how many guys have you had?"
+print "how many cats have you had?"
 guys = raw_input()
 
-print "so you've had %s guys and %s girls" % (guys, girls)
+print "so you've had %s dogs and %s cats" % (guys, girls)
